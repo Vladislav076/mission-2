12741131889adaecdb0c639fe565da83d199a3eb
Vladislav Densiov <denisov076@gmail.com>
2023-05-06 11:22:02 +0300

    Initial Commit

---
+++ mission 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mission 2
+//
+//  Created by Vladislav Denisov on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
